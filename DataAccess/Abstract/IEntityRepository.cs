2bb1f306d39d108adcaca6bfe343ef5a06efb100
Batuhan Aksüt <batuhanaksut@hotmail.com>
2024-04-07 18:46:59 +0300

    Add project files.

---
+++ DataAccess/Abstract/IEntityRepository.cs
@@ -0,0 +1,24 @@
+﻿using Castle.DynamicProxy.Generators.Emitters.SimpleAST;
+using Entities.Abstract;
+using Entities.Concrete;
+using Microsoft.Ajax.Utilities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Abstract
+{
+    //newlenebilir,IEntity alan bir class olmalı
+    public interface IEntityRepository<T> where T : class, IEntity,new()
+    {
+        List<T> GetAll(Expression<Func<T,bool>> filter= null);
+        T Get(Expression<Func<T, bool>> filter);
+        void Add(T entity);
+        void Update(T entity);
+        void Delete(T entity);
+
+    }
+}
