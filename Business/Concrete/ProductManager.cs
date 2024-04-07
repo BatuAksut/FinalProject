ceb116ce06fb02cb4d6dd1567cba2daee66f7add
Batuhan Aksüt <batuhanaksut@hotmail.com>
2024-04-07 20:11:14 +0300

    EntityFramework Added

--- Business/Concrete/ProductManager.cs
+++ Business/Concrete/ProductManager.cs
@@ -23,4 +23,15 @@ public class ProductManager : IProductService
     {
        return _productDal.GetAll();
     }
+
+
+    public List<Product> GetAllByCategoryId(int id)
+    {
+        return _productDal.GetAll(p=>p.CategoryID==id);
+    }
+
+    public List<Product> GetByUnitPrice(decimal min, decimal max)
+    {
+        return _productDal.GetAll(p=>p.UnitPrice>=min &&  p.UnitPrice <= max);
+    }
 }
