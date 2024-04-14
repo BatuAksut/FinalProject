b50e43f33a376462c9fea5ef2eac75a1311fefab
Batuhan Aksüt <batuhanaksut@hotmail.com>
2024-04-14 16:15:46 +0300

    Core Added

--- Core/DataAccess/IEntityRepository.cs
+++ Core/DataAccess/IEntityRepository.cs
@@ -1,7 +1,5 @@
-﻿using Castle.DynamicProxy.Generators.Emitters.SimpleAST;
-using Entities.Abstract;
-using Entities.Concrete;
-using Microsoft.Ajax.Utilities;
+﻿
+using Core.Entities;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,7 +7,7 @@ using System.Linq.Expressions;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace DataAccess.Abstract
+namespace Core.DataAccess
 {
     //newlenebilir,IEntity alan bir class olmalı
     public interface IEntityRepository<T> where T : class, IEntity,new()
