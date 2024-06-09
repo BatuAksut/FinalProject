c5632d5ab9e4d2779a74b1f3a5a29651c525eab1
Batuhan Aksüt <batuhanaksut@hotmail.com>
2024-06-09 22:24:52 +0300

    rental added

--- Core/Utilities/Results/IResult.cs
+++ Core/Utilities/Results/IResult.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace Core.Utilities.Results
 {
-    //For Basic Results
+    //For Basic Results - For voids
     public interface IResult
     {
         bool Success { get; }
