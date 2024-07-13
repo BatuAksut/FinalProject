89425577f3e852f61ad8fc7a201b1cb4d6864498
Batuhan Aksüt <batuhanaksut@hotmail.com>
2024-07-13 15:02:36 +0300

    IoC and API integration

--- Business/Concrete/ProductManager.cs
+++ Business/Concrete/ProductManager.cs
@@ -34,7 +34,7 @@ public class ProductManager : IProductService
 
     public IDataResult<List<Product>> GetAll()
     {
-        if (DateTime.Now.Hour == 22)
+        if (DateTime.Now.Hour == 15)
         {
             return new ErrorDataResult<List<Product>>(Messages.MaintenanceTime);
         }
