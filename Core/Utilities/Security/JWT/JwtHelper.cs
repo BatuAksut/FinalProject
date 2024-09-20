84086e94fdfaa4f68f459e65d59302b7805b55d1
Batuhan Aksüt <batuhanaksut@hotmail.com>
2024-09-20 17:23:40 +0300

    Login,register features optimized

--- Core/Utilities/Security/JWT/JwtHelper.cs
+++ Core/Utilities/Security/JWT/JwtHelper.cs
@@ -55,15 +55,19 @@ namespace Core.Utilities.Security.JWT
 			return jwt;
 		}
 
-		private IEnumerable<Claim> SetClaims(User user, List<OperationClaim> operationClaims)
-		{
-			var claims = new List<Claim>();
-			claims.AddNameIdentifier(user.Id.ToString());
-			claims.AddEmail(user.Email);
-			claims.AddName($"{user.FirstName} {user.LastName}");
-			claims.AddRoles(operationClaims.Select(c => c.Name).ToArray());
+        private IEnumerable<Claim> SetClaims(User user, List<OperationClaim> operationClaims)
+        {
+            var claims = new List<Claim>
+    {
+        new Claim(JwtRegisteredClaimNames.Sub, user.Email),
+        new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString()),
+        new Claim("fullName", $"{user.FirstName} {user.LastName}") // Tam ad olarak ekleyin
+    };
+            claims.AddRoles(operationClaims.Select(c => c.Name).ToArray());
+            return claims;
+        }
 
-			return claims;
-		}
-	}
+
+
+    }
 }
